96cf9d80f2efff0a5ef6b214d249f5fed9ec2a4e
Oleg Matus <matys_bboy96@ukr.net>
2023-07-04 18:58:22 +0300

    creation components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Task from "./components/Task";
 
 function App() {
   return (
     <div className="App">
-     <Task/>
+
     </div>
   );
 }
